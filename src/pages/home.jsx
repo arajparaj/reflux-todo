6dbcb265ffc95a5ccb40c906d1c174d040f3a2f5
Araj P Raju <arajparaj@gmail.com>
2016-01-31 16:05:26 +0530

    Hello World

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react'
+import Reflux from 'reflux';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default Home;
--- webpack-config.js
+++ webpack-config.js
@@ -1,10 +1,9 @@
-const path              = require('path')
-const webpack           = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    './src/app'
+    'webpack-hot-middleware/client?reload=true', './src/app'
   ],
   devtool: "eval",
   debug: true,
@@ -17,18 +16,27 @@ module.exports = {
     modulesDirectories: ['node_modules']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin()
   ],
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js','.jsx']
   },
   module: {
     loaders: [
-      {test: /.*\.json$/, loader: 'json'},
-      {test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/, loader: "file-loader"},
-      {test: /\.js$/, loader: 'babel-loader' }
+      {
+        test: /.*\.json$/,
+        loader: 'json'
+      }, {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: "file-loader"
+      }, {
+        test: /\.js?x$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [require.resolve('babel-preset-es2015'), require.resolve('babel-preset-react'), require.resolve('babel-preset-stage-0')]
+        }
+      }
     ]
   }
 }
-
